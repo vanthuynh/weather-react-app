eb13172ebc5c96d2595cfc864be3511bd9452069
Van Huynh <vthuynh@cpp.edu>
2022-07-19 00:04:59 -0400

    add search functionality and GET request

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '208fc101a5msha0c46653198e27cp1dd005jsn96376c37f5b2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
